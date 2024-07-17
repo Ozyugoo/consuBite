ee6d2016cb26d5beb835e19bb763709af55ada39
Ozyugoo <stellaozy22@gmail.com>
2024-07-18 00:22:26 +0100

    Chore: Make header responsive

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,11 +4,11 @@ import Navbar from "./Navbar.jsx";
 function Header() {
   return (
     <>
-      <div className="flex">
-        <div>
+      <div className="flex justify-between items-center py-6 px-6 md:px-8 lg:px-12 xl:px-24 w-full">
+        <div className="md:w-2/6 xl:w-2/5 pl-7 lg:pl-0">
           <img src={Logo} alt="ConsuBite Logo" />
         </div>
-        <div>
+        <div className="md:w-4/6 xl:w-3/5">
           <Navbar />
         </div>
       </div>
--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -1,58 +1,66 @@
 import Icon from "../../assets/headerIcon.svg";
-
 import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Link as Scroll } from "react-scroll";
+import Button from "../Button";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
+  function toggleMenu() {
     setIsOpen(!isOpen);
-  };
+  }
 
-  const closeMenu = () => {
+  function closeMenu() {
     setIsOpen(false);
-  };
+  }
 
   return (
-    <div className="nav">
-      <nav>
-        <div className="btn-menu">
+    <div>
+      <nav
+        className={`flex items-center justify-between cursor-pointer w-full md:text-sm `}
+      >
+        <div className="block md:hidden">
           <div onClick={toggleMenu}>
             <svg
-              className="hambugBar"
+              className=""
               width="44"
               height="40"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path d="M6 16h32M6 28h32" stroke="#C7C7C7" strokeWidth="2" />
+              <path d="M6 16h32M6 28h32" stroke="#000" strokeWidth="2" />
             </svg>
           </div>
         </div>
-        <ul className={`md:flex md:visible ${isOpen ? "block" : "hidden"}`}>
-          <p onClick={closeMenu} className="toggle">
+        <ul
+          className={`md:flex md:w-4/6 space-x-6 lg:text-base font-semibold md:p-0 cursor-pointer md:text-black p-10 absolute lg:relative right-0 items-center md:relatve md:bg-inherit sm:z-50 top-0 w-1/2 lg:w-3/5 lg:flex ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <p
+            onClick={closeMenu}
+            className="md:hidden hover:text-red-500 focus:text-red-500 active:text-red-500"
+          >
             &#10006;
           </p>
-          <li>
+          <li className="hover:text-red-500 focus:text-red-500 active:text-red-500 focus:font-bold m-6">
             <Link to="/" onClick={closeMenu}>
               Home
             </Link>
           </li>
-          <li>
+          <li className="hover:text-red-500 focus:text-red-500 active:text-red-500 focus:font-bold m-6">
             <Link to="/Menu" onClick={closeMenu}>
               Menu
             </Link>
           </li>
-          <li>
+          <li className="hover:text-red-500 focus:text-red-500 active:text-red-500 focus:font-bold m-6">
             <Link to="/about" onClick={closeMenu}>
               About Us
             </Link>
           </li>
-          <li>
+          <li className="hover:text-red-500 focus:text-red-500 active:text-red-500 focus:font-bold m-6">
             <Scroll
-              className="navList"
               onClick={closeMenu}
               activeClass="active"
               to="contact"
@@ -64,9 +72,32 @@ function Navbar() {
               Contact
             </Scroll>
           </li>
+          <li className="lg:hidden m-6">
+            <Button className="bg-white border border-red-500 hover:bg-red-500 text-red-500 hover:text-white focus:bg-red-500 focus:text-white active:bg-red-500 active:text-white py-2 px-6">
+              Sign Up
+            </Button>
+          </li>
+          <li className="lg:hidden m-6">
+            <Button className="bg-white border border-red-500 hover:bg-red-500 text-red-500 hover:text-white focus:bg-red-500 focus:text-white active:bg-red-500 active:text-white py-2 px-5">
+              Login
+            </Button>
+          </li>
         </ul>
-        <img src={Icon} alt="Pot Icon" />
+        <div className="flex md:w-1/2 justify-between space-x-6 items-center">
+          <img
+            src={Icon}
+            alt="Pot Icon"
+            className="pl-3 absolute left-0 lg:relative"
+          />
+          <Button className="hidden lg:flex bg-white border border-red-500 hover:bg-red-500 text-red-500 hover:text-white focus:bg-red-500 focus:text-white active:bg-red-500 active:text-white py-2 px-6">
+            Sign Up
+          </Button>
+          <Button className="hidden lg:flex bg-white border border-red-500 hover:bg-red-500 text-red-500 hover:text-white focus:bg-red-500 focus:text-white active:bg-red-500 active:text-white py-2 px-5">
+            Login
+          </Button>
+        </div>
       </nav>
+
       <Outlet />
     </div>
   );
