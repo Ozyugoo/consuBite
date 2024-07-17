cdaaaa241ffcb75f835b590bcb978de646d39937
Queen <chukuqueen55@gmail.com>
2024-07-17 20:13:22 +0100

    chore: update layout and styling for what we serve section with custom colors and responsiveness

--- src/whatServe/whatWeServe.jsx
+++ src/whatServe/whatWeServe.jsx
@@ -9,6 +9,11 @@ function WhatWeServe() {
                     <div className="w-full sm:w-1/2 mb-8 sm:mb-8">
                         <img src={whatWeServeImage} alt="whatWeServeImage" className="w-full h-auto" />
                     </div>
+                    <div className="m-20 w-full sm:w-1/2">
+                        <h1 className="text-4xl sm:text-5xl font-bold mb-6">What We <span className='text-custom-red'>Serve</span></h1>
+                        <p className="text-sm sm:text-base font-normal mb-4">Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec. Morbi ut tincidunt eu tempus amet at sit. Bibendum tortor vitae convallis diam pellentesque eu est mattis. Vestibulum sit habitant non vitae.</p>
+                        <p className="text-sm sm:text-base font-normal">Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec. Morbi ut tincidunt eu tempus amet at sit. Bibendum tortor vitae convallis diam pellentesque eu est mattis. Vestibulum sit habitant non vitae.</p>
+                    </div>
                 </div>
             </div>
         </div>
