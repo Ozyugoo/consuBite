687dbb88628a2d65fbab7f1700e4fee15beb7694
Ivy M. W. <111966676+SheCode287@users.noreply.github.com>
2024-07-20 22:24:32 +0300

    Heroes (#5)
    * chore:Heroes Section
    
    * chore:updated my Hero file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar.jsx";
 
 function Header() {
   return (
-    <>
+    < >
       <div className="flex justify-between items-center py-6 lg:py-4 px-6 md:px-10 lg:px-20 xl:px-24 w-full bg-custom-light h-24 mb-6">
         <div className="md:w-2/6 xl:w-2/5 pl-7 lg:pl-0">
           <img src={Logo} alt="ConsuBite Logo" />
