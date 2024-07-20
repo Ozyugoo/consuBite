62a88e70d848a1b8495d08c8af17c43a7ce2e1aa
Wokrit <bakkuk7@gmail.com>
2024-07-20 17:43:33 +0100

    chore - install and configure firebase on consuBite

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // If you're using Firestore
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"; // If you're using Authentication
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: import.meta.env.VITE_API_KEY,
-   authDomain: import.meta.env.VITE_DOMAIN,
-   projectId: import.meta.env.VITE_PROJECT_ID,
-   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-   messagingSenderId: import.meta.env.VITE_SENDER_ID,
-   appId: import.meta.env.VITE_APP_ID,
-   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
- };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-export { analytics, db, auth, googleProvider, signInWithPopup };
+export const auth = getAuth(app);
+export default app;
