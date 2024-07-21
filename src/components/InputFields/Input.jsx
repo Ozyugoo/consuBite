d354e177e7fe14d173d85234a0a5af0d1a267de1
Wokrit <bakkuk7@gmail.com>
2024-07-22 00:06:53 +0100

    feat - create input field component for froms

---
+++ src/components/InputFields/Input.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+function Input({
+  label,
+  type = "text",
+  value,
+  onChange,
+  className,
+  placeholder,
+  width = "w-full",
+}) {
+  return (
+    <div className={`flex flex-col gap-[10px] font-poppins ${className}`}>
+      <label className={`text-sm font-normal text-custom-black`}>{label}</label>
+      <div className={`relative ${width}`}>
+        <input
+          className={` placeholder-custom-grey border border-custom-black-8 rounded-[20px] px-[16px] text-sm font-normal text-custom-black ${width} h-[41px] hover:border-custom-grey  focus:border-custom-black `}
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Input;
