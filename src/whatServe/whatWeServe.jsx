f30e2d9a7bc59954cced3ed9ff548345e7739f51
Queen <chukuqueen55@gmail.com>
2024-07-22 06:15:29 +0100

    style: adjust media queries for consistency

--- src/whatServe/whatWeServe.jsx
+++ src/whatServe/whatWeServe.jsx
@@ -20,39 +20,3 @@ function WhatWeServe() {
 }
 
 export default WhatWeServe;
-
-// import whatWeServeImage from "../assets/images/whatWeServe.png";
-
-// function WhatWeServe() {
-//   return (
-//     <div className="h-screen flex items-center justify-center px-6 sm:px-32 py-6 sm:py-0">
-//       <div className="h-screen bg-gray-50 flex items-center">
-//         <section
-//           className="bg-cover bg-center py-32 w-full">
-//           <div className="container mx-auto text-left text-white">
-//             <div className="flex items-center">
-//               <div className="w-1/2">
-//                 <h1 className="text-5xl font-medium mb-6">Welcome to My Agency</h1>
-//                 <p className="text-xl mb-12">
-//                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-//                   viverra euismod odio, gravida pellentesque urna varius vitae.
-//                 </p>
-//                 <a
-//                   href="#"
-//                   className="bg-indigo-500 text-white py-4 px-12 rounded-full hover:bg-indigo-600"
-//                 >
-//                   Demo
-//                 </a>
-//               </div>
-//               <div className="w-1/2 pl-16">
-//               <img src={whatWeServeImage} alt="whatWeServeImage" className="w-full h-auto" />
-//               </div>
-//             </div>
-//           </div>
-//         </section>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default WhatWeServe;
