b550817c202f2a4ff2f550032e79c070a7a0df23
Queen <chukuqueen55@gmail.com>
2024-07-25 11:11:10 +0100

    feat: What customers say component

---
+++ src/components/whatcustomerSay/index.jsx
@@ -0,0 +1,12 @@
+function WhatCustomersSay() {
+    return(
+        <div>
+            <div>
+                <h1 className="text-4xl sm:text-5xl font-bold mb-6">What Our <span className='text-custom-red'>Customers</span> Say</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default WhatCustomersSay
