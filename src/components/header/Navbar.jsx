3f3f9b57071d7817232024fa6039654d82e9ef18
Stella Ozioma Ugoo-Okonkwo <113618136+Ozyugoo@users.noreply.github.com>
2024-08-21 19:52:52 +0100

    Modified Header (#16)
    * Chore: update Header and navBar
    
    * Chore: Update nav and move whatWeServe to components
    
    * Chore: Update Signup
    
    * Chore: Update header

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,6 +2,7 @@ import Icon from "../../assets/headerIcon.svg";
 import { useState, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Link as Scroll } from "react-scroll";
+import Logo from "../../assets/ConsuBiteLogo.svg";
 import Button from "../buttons/Button";
 import userAuthContext from "../../pages/authentication/context/AuthContext";
 
@@ -17,14 +18,17 @@ function Navbar() {
   function closeMenu() {
     setIsOpen(false);
   }
-
   return (
-    <div>
-      <nav
-        className={`flex items-center justify-between cursor-pointer w-full md:text-sm `}
-      >
-        <div className="block md:hidden absolute left-0 lg:relative">
-          <div onClick={toggleMenu}>
+    <>
+      <div className="flex justify-between items-center space-x-6 px-16 md:px-12 2xl:px-24 md:py-6">
+        <div className="w-full justify-center items-center pl-12 lg:pl-0">
+          <img src={Logo} alt="ConsuBite Logo" />
+        </div>
+        <div className="w-full flex items-center">
+          <div
+            onClick={toggleMenu}
+            className="block absolute left-8 cursor-pointer lg:hidden"
+          >
             <svg
               className=""
               width="44"
@@ -35,109 +39,112 @@ function Navbar() {
               <path d="M6 16h32M6 28h32" stroke="#000" strokeWidth="2" />
             </svg>
           </div>
-        </div>
-        <ul
-          className={`bg-custom-black md:flex md:w-4/6 md:px-6 lg:space-x-5 md:space-x-5 xl:text-base justify-center text-custom-light font-semibold md:p-0 cursor-pointer sm:bg-custom-black md:text-custom-black md:bg-custom-white p-10 absolute lg:relative left-0 items-center md:relatve top-0 sm:h-screen md:h-[6rem] lg:h-20  w-1/2 ${
-            isOpen ? "block" : "hidden"
-          }`}
-        >
-          <p
-            onClick={closeMenu}
-            className="md:hidden hover:text-red-500 focus:text-red-500 active:text-red-500"
-          >
-            &#10006;
-          </p>
-          <li className="hover:text-custom-red focus:text-custom-red active:text-custom-red focus:font-bold m-6">
-            <Link to="/" onClick={closeMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="hover:text-custom-red focus:text-custom-red active:text-custom-red focus:font-bold m-6">
-            <Link to="/Menu" onClick={closeMenu}>
-              Menu
-            </Link>
-          </li>
-          <li className="hover:text-custom-red focus:text-custom-red active:text-custom-red focus:font-bold m-6">
-            <Link to="/about" onClick={closeMenu}>
-              About Us
-            </Link>
-          </li>
-          <li className="hover:text-custom-red focus:text-custom-red active:text-custom-red focus:font-bold m-6">
-            <Scroll
-              onClick={closeMenu}
-              activeClass="active"
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Contact
-            </Scroll>
-          </li>
+          <div className="md:flex md:justify-center md:items-center md:h-5 py-5">
+            <nav className="cursor-pointer flex justify-center items-center">
+              <ul
+                className={`max-[780px]:bg-custom-black absolute top-0 left-0 w-1/2 lg:w-1/3 lg:flex justify-between items-center max-[780px]h-screen md:relative lg:mr-4 text-custom-light font-semibold md:text-custom-black p-10 ${
+                  isOpen ? "block" : "hidden"
+                }`}
+              >
+                <p onClick={closeMenu} className="lg:hidden pb-5">
+                  &#10006;
+                </p>
+                <li className="hover:text-custom-redHover focus:text-custom-redFocus active:text-custom-red focus:font-bold m-6">
+                  <Link to="/" onClick={closeMenu}>
+                    Home
+                  </Link>
+                </li>
+                <li className="hover:text-custom-redHover focus:text-custom-redFocus active:text-custom-red focus:font-bold m-6">
+                  <Link to="/Menu" onClick={closeMenu}>
+                    Menu
+                  </Link>
+                </li>
+                <li className="hover:text-custom-redHover focus:text-custom-redFocus active:text-custom-red focus:font-bold m-6">
+                  <Link to="/about" onClick={closeMenu}>
+                    About
+                  </Link>
+                </li>
+                <li className="hover:text-custom-redHover focus:text-custom-redFocus active:text-custom-red focus:font-bold m-6 ">
+                  <Scroll
+                    onClick={closeMenu}
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                  >
+                    Contact
+                  </Scroll>
+                </li>
 
-          {isLoggedIn ? (
-            <li className="lg:hidden m-6">
-              <Button variant="primary" className="px-8">
-                Logout
-              </Button>
-            </li>
-          ) : (
-            <>
-              <li className="lg:hidden m-6">
-                <Button
-                  variant="secondary"
-                  onClick={openSignUpModal}
-                  className="px-8"
-                >
-                  Sign Up
-                </Button>
-              </li>
-              <li className="lg:hidden m-6">
-                <Button
-                  variant="primary"
-                  className="px-8"
-                  onClick={openLoginModal}
-                >
-                  Login
-                </Button>
-              </li>
-            </>
-          )}
-        </ul>
-        <div className="flex md:w-1/2 gap-4 space-x-3 items-center justify-end">
-          <img src={Icon} alt="Pot Icon" className="relative" />
-          {isLoggedIn ? (
-            <Button
-              variant="primary"
-              className="hidden lg:flex px-8"
-              onClick={openLoginModal}
-            >
-              Logout
-            </Button>
-          ) : (
-            <>
-              <Button
-                variant="secondary"
-                onClick={openSignUpModal}
-                className="hidden lg:flex px-8"
-              >
-                Sign Up
-              </Button>
-              <Button
-                variant="primary"
-                className="hidden lg:flex px-8"
-                onClick={openLoginModal}
-              >
-                Login
-              </Button>
-            </>
-          )}
+                {isLoggedIn ? (
+                  <li className="lg:hidden m-6">
+                    <Button variant="primary" className="px-8">
+                      Logout
+                    </Button>
+                  </li>
+                ) : (
+                  <>
+                    <li className="md:hidden m-6">
+                      <Button
+                        variant="secondary"
+                        onClick={openSignUpModal}
+                        className="px-8"
+                      >
+                        Sign Up
+                      </Button>
+                    </li>
+                    <li className="md:hidden m-6 ">
+                      <Button
+                        variant="primary"
+                        className="px-8"
+                        onClick={openLoginModal}
+                      >
+                        Login
+                      </Button>
+                    </li>
+                  </>
+                )}
+              </ul>
+              <div className="flex md:w-full md:space-x-4 justify-end items-center md:ml-8 lg:ml-4">
+                <div className="">
+                  <img src={Icon} alt="Pot Icon" className="ml-48 md:ml-0" />
+                </div>
+                {isLoggedIn ? (
+                  <Button
+                    variant="primary"
+                    className="hidden md:flex px-8"
+                    onClick={openLoginModal}
+                  >
+                    Logout
+                  </Button>
+                ) : (
+                  <div className="flex justify-between items-center space-x-6">
+                    <Button
+                      variant="secondary"
+                      onClick={openSignUpModal}
+                      className="hidden md:flex px-8 md:text-sm md:font-bold"
+                    >
+                      Sign Up
+                    </Button>
+                    <Button
+                      variant="primary"
+                      className="hidden md:flex px-8"
+                      onClick={openLoginModal}
+                    >
+                      Login
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </nav>
+          </div>
         </div>
-      </nav>
+      </div>
 
       <Outlet />
-    </div>
+    </>
   );
 }
 
