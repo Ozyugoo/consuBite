0902d293904021bd46dbac28da17f854a2d18a70
Wokrit <bakkuk7@gmail.com>
2024-08-24 15:54:45 +0100

    feat - create a js file containing an array of meals to be displayed on menu page

---
+++ src/pages/menu/meals.js
@@ -0,0 +1,49 @@
+const meals = [
+  {
+    id: 1,
+    photo: "/pictures/Meal 1.png",
+    name: "Spaghetti Bolognese",
+    rating: 3.2,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec",
+    price: 2500,
+  },
+  {
+    id: 2,
+    photo: "/pictures/Meal 1.png",
+    name: "Spaghetti Bolognese",
+    rating: 3.0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec",
+    price: 2500,
+  },
+  {
+    id: 3,
+    photo: "/pictures/Meal 1.png",
+    name: "Spaghetti Bolognese",
+    rating: 3.0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec",
+    price: 2500,
+  },
+  {
+    id: 4,
+    photo: "/pictures/Meal 1.png",
+    name: "Spaghetti Bolognese",
+    rating: 3.0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec",
+    price: 2500,
+  },
+  {
+    id: 5,
+    photo: "/pictures/Meal 1.png",
+    name: "Spaghetti Bolognese",
+    rating: 3.0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sem at nullam aliquam quam rhoncus amet at lobortis nec",
+    price: 2500,
+  },
+];
+
+export default meals;
