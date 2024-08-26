fed04cd5b6f281fba0979ea12133e8ced6a04127
Wokrit <bakkuk7@gmail.com>
2024-08-26 17:01:32 +0100

    feat - add filter pills to filter meals according to category

---
+++ src/pages/menu/MealFilters.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const categories = [
+  "All",
+  "Pasta",
+  "Rice",
+  "Swallow",
+  "Soups",
+  "Grills",
+  "Protein",
+  "Snacks",
+  "Drinks",
+  "Side dishes",
+];
+
+function MealFilters({ onCategoryChange }) {
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  function handleCategoryClick(category) {
+    setActiveCategory(category);
+    onCategoryChange(category);
+  }
+
+  return (
+    <div className="flex flex-row gap-3 ">
+      {categories.map((category) => (
+        <div
+          key={category}
+          onClick={() => handleCategoryClick(category)}
+          className={` bg-custom-white drop-shadow-sm px-6 py-1.5 rounded-2xl text-sm font-normal text-custom-greyDark cursor-pointer ${
+            activeCategory === category
+              ? "bg-secondary-focus text-custom-red border border-custom-red"
+              : ""
+          }`}
+        >
+          {category}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MealFilters;
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import meals from "./meals";
 import MealContainer from "./MealContainer";
+import MealFilters from "./MealFilters";
 
 function Menu() {
+  const [filteredMeals, setFilteredMeals] = useState(meals);
+
+  function handleCategoryChange(category) {
+    if (category === "All") {
+      setFilteredMeals(meals);
+    } else {
+      setFilteredMeals(meals.filter((meal) => meal.category === category));
+    }
+  }
   return (
     <>
       <div className="bg-custom-light h-full w-full px-20 py-8 flex flex-col gap-8 items-center">
         <div className="text-[32px] font-bold">Menu</div>
+        <MealFilters onCategoryChange={handleCategoryChange} />
         <div className="grid grid-cols-4 gap-5 font-poppins">
-          {meals.map((meals) => (
+          {filteredMeals.map((meals) => (
             <MealContainer mealObject={meals} key={meals.id} />
           ))}
         </div>
