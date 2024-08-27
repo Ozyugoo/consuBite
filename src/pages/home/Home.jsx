b4dc74551bf2754a1a6bfb3d452c55b8e827108c
SheCode287 <ivy.muthoni.wanjohi@gmail.com>
2024-08-27 16:31:46 +0300

    updated folder

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
-import Hero from "../../components/Hero";
+// import Hero from "../../components/Hero";
 import WhatWeServe from "../../components/whatServe/whatWeServe";
-import Ourservices from "../../components/Ourservices";
+// import Ourservices from "../../components/Ourservices";
+import Ourservices from "../../components/ourServices/Ourservices";
+import Hero from "../../components/hero/Hero";
 import Footer from "../../components/footer/Footer";
 
 function Home() {
