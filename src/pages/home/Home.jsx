b652d032376793298f60e64cebd9818445aab4fe
Ozyugoo <stellaozy22@gmail.com>
2024-08-31 22:10:34 +0100

    Chore: Update home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,17 @@
-// import Hero from "../../components/Hero";
 import WhatWeServe from "../../components/whatServe/whatWeServe";
-// import Ourservices from "../../components/Ourservices";
 import Ourservices from "../../components/ourServices/Ourservices";
 import Hero from "../../components/hero/Hero";
 import Footer from "../../components/footer/Footer";
-import WhatCustomersSay from "../../WhatCustomersSay";
+import WhatCustomersSay from "../../components/WhatCustomersSay";
+
 function Home() {
   return (
     <>
       {/* <Header /> */}
-      <Hero />      
+      <Hero />
       <WhatWeServe />
       <Ourservices />
-      <WhatCustomersSay/>
+      <WhatCustomersSay />
       <Footer />
     </>
   );
