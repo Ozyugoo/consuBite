d68d0d8ece05ff98107ce6e23e3a9fd9d9d16f2f
Wokrit <bakkuk7@gmail.com>
2024-09-18 15:34:15 +0100

    feat: create functions to filter meals according to search keywords or categories

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -5,20 +5,30 @@ import MealFilters from "./MealFilters";
 import SearchBar from "../../components/searchBar/SearchBar";
 
 function Menu() {
-  const [filteredMeals, setFilteredMeals] = useState(meals);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+
+  // Filter meals based on search and category
+  const filteredMeals = meals.filter(
+    (meal) =>
+      (selectedCategory === "All" || meal.category === selectedCategory) &&
+      (meal.name.toLowerCase().includes(search.toLowerCase()) ||
+        meal.description.toLowerCase().includes(search.toLowerCase()))
+  );
 
   function handleCategoryChange(category) {
-    if (category === "All") {
-      setFilteredMeals(meals);
-    } else {
-      setFilteredMeals(meals.filter((meal) => meal.category === category));
-    }
+    setSelectedCategory(category);
   }
+
   return (
     <>
       <div className="bg-custom-light h-full w-screen py-8 flex flex-col gap-8 items-center lg:px-20 md:px-20 px-6">
         <div className="text-[32px] font-bold">Menu</div>
-        <SearchBar placeholder={"Search menu..."} />
+        <SearchBar
+          placeholder={"Search menu..."}
+          search={search}
+          setSearch={setSearch}
+        />
         <MealFilters onCategoryChange={handleCategoryChange} />
         <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 font-poppins">
           {filteredMeals.map((meals) => (
